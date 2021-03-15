f7a8932ffbd45a49c298b99d7df82e172f7c721e
SCHAIGNE <stephane.chaigne@gmail.com>
2021-03-15 22:06:04 +0100

    save

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,48 +7,101 @@ import { PrimeIcons } from 'primeng/api';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-
-  eventCours: any[] = []
+  eventCours: any[] = [];
+  eventPro: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
 
-    // $("#titre").hide(0);
-    // $("#titre").show("fade",2500);
-
     this.eventCours = [
       {
         status: 'BTS SIO',
-        date: 'D',
+        date: '2019 - 2021',
         icon: PrimeIcons.CALENDAR,
         color: '#212529',
-        messages: ["Culture Générale & Expression",
-          "Anglais",
-          "Mathématiques",
-          "Algorithmie",
-          "Analyse économique, managériale et juridiques des services informatiques",
-          "Enseignements Communs : 4 Modules au 1er semestre et 2 au second",
-          "Enseignements de spécialité (SLAM ou SISR) : 2 modules uniquement au second semestre",
-          "Projets Personnels Encadrés (PPE)",
+        messages: ["BTS Service Informatiques aux Organisation - Lycée Carcouet / Nantes",
         ]
       },
       {
-        status: 'Deuxième Année',
-        date: 'De BTS SIO',
+        status: 'BTS Electrotechnique',
+        date: '2017 - 2019',
         icon: PrimeIcons.CALENDAR,
         color: '#212529',
-        messages: ["Culture Générale & Expression",
-          "Anglais",
-          "Mathématiques",
-          "Algorithmie",
-          "Analyse économique, managériale et juridiques des services informatiques",
-          "Enseignements Communs : 1 Module de Gestion de Projet",
-          "Enseignements de spécialité (SLAM ou SISR) : 3 Modules",
-          "Projets Personnels Encadrés (PPE)",
+        messages: ["BTS Electrotechnique en apprentissage – CFA Martello / Nantes",
         ]
       },
-    ];
-
+      {
+        status: 'BAC PRO ELEEC',
+        date: '2014 - 2017',
+        icon: PrimeIcons.CALENDAR,
+        color: '#212529',
+        messages: ["BAC PRO Electrotechnique, énergie, équipements communicants en apprentissage (Mention Bien) – CFA Martello/ Nantes",
+        ]
+      },
+      {
+        status: 'BEP ELEEC',
+        date: '2014 - 2016',
+        icon: PrimeIcons.CALENDAR,
+        color: '#212529',
+        messages: ["BEP Electrotechnique, énergie, équipements communicants en apprentissage – CFA Martello/ Nantes",
+        ]
+      },
+      {
+        status: 'PSC1',
+        date: '2014',
+        icon: PrimeIcons.CALENDAR,
+        color: '#212529',
+        messages: ["Prévention Secours Civique niveau 1 - Lycée Aristide Briand / Saint Nazaire",
+        ]
+      },
+      {
+        status: 'Brevet',
+        date: '2010 - 2013',
+        icon: PrimeIcons.CALENDAR,
+        color: '#212529',
+        messages: ["Diplôme national du brevet (Mention assez bien) – Collège Jean Moulin / Saint-Nazaire",
+        ]
+      },
+    ]
+    this.eventPro = [
+      {
+        status: 'CSMSI - La Poste',
+        date: '2020 - 20**',
+        icon: PrimeIcons.CALENDAR,
+        color: '#212529',
+        messages: ["Developpement Angular",
+          "Refonte du desgin de Vehiparc",
+          "UI/UX de l'aide de Vehiparc",
+          "Méthode Agile - SCRUM",
+        ]
+      },
+      {
+        status: 'Apprenti BTS Electricien / EES - Clemessy',
+        date: '2017-2019',
+        icon: PrimeIcons.CALENDAR,
+        color: '#212529',
+        messages: ["Raccordements de câbles pour l’automatisation de procédé",
+        "Paramétrage d’équipements électriques",
+        "Tirage de câbles",
+        "Organisation de chantiers (Matériels à prévoir, gestion de personnelet du chantier en général)",
+        "Créationde programmes automates sous UnityPro XL (au CFA)",
+        "Travaux dans des zones agro-alimentaires (ALVA, Maitre-Coq)",
+        "Gestion de personnel lors de salons, au parc expositions de la Beaujoire à Nantes",
+        ]
+      },
+      {
+        status: 'Apprenti Bac PROElectricien / STXFRANCE – Saint-Nazaire',
+        date: '2014-2017',
+        icon: PrimeIcons.CALENDAR,
+        color: '#212529',
+        messages: ["Raccordements de différents appareils etarmoires électriques",
+        "Raccordements d’armoires électriques de commandes suivant les schémas fournispar le bureau d’études.",
+        "Mise en place de chemins de câbles (Cablofil, Cornière, Télex)",
+        "Soudure à l’arc",
+        "Tirage de câbles sur navires militaires (Sébastopol, Vladivostok…) , navires de croisières."
+        ]
+      },
+    ]
   }
 }
